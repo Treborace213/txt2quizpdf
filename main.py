da34db30931f3138a289bab92975f93463bc110d
Robert Dorling <RobbieDorling03@gmail.com>
2025-06-06 15:11:39 +0100

    Program now takes a txt file and turns it to a pdf.

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import sys
 import os
+from pathlib import Path
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
 
 def main():
     if len(sys.argv) < 2:
         print("Usage: python main.py <path_to_input_txt>")
         sys.exit(1)
 
-    input_path = sys.argv[1]
-    target_path = os.path.normpath(os.path.expandvars(os.path.expanduser(input_path)))
+    input_path_str = sys.argv[1]
+    target_path_str = os.path.normpath(os.path.expandvars(os.path.expanduser(input_path_str)))
 
-    if not os.path.isfile(target_path):
-        print(f"Error: File does not exist -> {target_path}")
+    if not os.path.isfile(target_path_str):
+        print(f"Error: File does not exist -> {target_path_str}")
         sys.exit(1)
 
-    print(f"Input file path: {target_path}")
+    target_path = Path(target_path_str)
+
+    doc_name = target_path.stem
+
+    doc = SimpleDocTemplate(doc_name + ".pdf")
+    styles = getSampleStyleSheet()
+
+    flow = []
+
+    flow.append(Paragraph(doc_name, styles["Title"]))
+    flow.append(Spacer(1,12))
+
+    with open(target_path, 'r') as text_file:
+        for line in text_file:
+            flow.append(Paragraph(line.strip(), styles["Normal"]))
+    
+    doc.build(flow)
+    print("Generated pdf.")
+
+
 
 if __name__ == "__main__":
     main()
